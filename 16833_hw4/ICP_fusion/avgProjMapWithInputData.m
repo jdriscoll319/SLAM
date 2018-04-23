f1017c03e9cdb864d7d959a3d70e5c2fb791b2cd
Adam Driscoll <jdriscol@andrew.cmu.edu>
2018-04-22 20:34:51 -0400

    writeup complete and submitted

--- 16833_hw4/ICP_fusion/avgProjMapWithInputData.m
+++ 16833_hw4/ICP_fusion/avgProjMapWithInputData.m
@@ -17,6 +17,8 @@ function updated_map = avgProjMapWithInputData(proj_map, input_data, alpha, h, w
     counts_3d = repmat(proj_ccounts, [1 1 3]);
     is_use_3d = repmat(is_use, [1 1 3]);
     alpha_3d = repmat(alpha, [1 1 3]);
+    proj_colors = double(proj_colors);
+    input_colors = double(input_colors);
 
     
     %%
@@ -26,11 +28,11 @@ function updated_map = avgProjMapWithInputData(proj_map, input_data, alpha, h, w
         (counts_3d(is_use_3d).*proj_points(is_use_3d) + alpha_3d(is_use_3d).*input_points(is_use_3d)) ...
         ./ (counts_3d(is_use_3d) + alpha_3d(is_use_3d));
     
-%     updated_colors = proj_colors;
-%     updated_colors(is_use_3d) = ...
-%         (counts_3d(is_use_3d).*proj_colors(is_use_3d) + alpha_3d(is_use_3d).*input_colors(is_use_3d)) ...
-%         ./ (counts_3d(is_use_3d) + alpha_3d(is_use_3d));
-    updated_colors = input_colors;
+    updated_colors = proj_colors;
+    updated_colors(is_use_3d) = ...
+        (counts_3d(is_use_3d).*proj_colors(is_use_3d) + alpha_3d(is_use_3d).*input_colors(is_use_3d)) ...
+        ./ (counts_3d(is_use_3d) + alpha_3d(is_use_3d));
+%     updated_colors = input_colors;
     
     updated_normals = proj_normals;
     updated_normals(is_use_3d) = ...
@@ -41,7 +43,7 @@ function updated_map = avgProjMapWithInputData(proj_map, input_data, alpha, h, w
     updated_ccounts(is_use) = proj_ccounts(is_use) + alpha(is_use);
     
     updated_times = proj_times;
-    updated_times(is_use) = proj_times(is_use) + t;
+    updated_times(is_use) = t;%proj_times(is_use) + t;
         
     %==== Output the updated projected map in a struct ====
     updated_map = struct('points', updated_points, 'colors', updated_colors, 'normals', updated_normals, 'ccounts', updated_ccounts, 'times', updated_times);
